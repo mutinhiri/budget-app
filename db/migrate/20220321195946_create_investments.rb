59765afc4afa206e90d23b8fe2a969e11c38c6e1
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-03-21 22:01:55 +0200

    Add table invest

--- db/migrate/20220321195946_create_investments.rb
+++ db/migrate/20220321195946_create_investments.rb
@@ -1,6 +1,8 @@
 class CreateInvestments < ActiveRecord::Migration[7.0]
   def change
     create_table :investments do |t|
+      t.string :name
+      t.float :amount
 
       t.timestamps
     end
