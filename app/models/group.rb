4b4069bf2c2e2596378eeca88c2819c235d55ff5
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-03-22 20:31:59 +0200

    Fix bug in group association

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
   belongs_to :user
   has_many :investments
-  validates :name, presense: true
-  validates :icon, presense: true
+  validates :name, presence: true
+  validates :icon, presence: true
 end
