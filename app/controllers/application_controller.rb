42b3146053aea0500c1b88aa162121c82a9fd55d
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-03-22 17:44:07 +0200

    Default parameter check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,4 +19,5 @@ class ApplicationController < ActionController::Base
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
+    devise_parameter_sanitizer
 end
