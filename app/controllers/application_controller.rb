7a95db65077142ea08f4cc668a5b36aea80aaf06
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-03-22 17:44:40 +0200

    Update account after load

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,5 +19,5 @@ class ApplicationController < ActionController::Base
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
-    devise_parameter_sanitizer
+    devise_parameter_sanitizer.permit(:account_update)
 end
