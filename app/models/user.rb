23ae97f16874d59b0aa7ddee9aef730c4861522e
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-03-22 17:35:10 +0200

    Add user properties

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, :email, presence: true
+  validates :email, uniqueness: true
+  has_many :groups, dependent: :destroy
+  has_many :investments, dependent: :destroy
 end
