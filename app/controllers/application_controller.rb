425054130bbf5731fb89ba5886ced7a7d78f2d9d
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-03-22 17:38:06 +0200

    Authenticate user and update parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :check_page
+  before_action :update_allowed_parameters, if: :devise_controller?
 end
