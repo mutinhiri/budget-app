8c521aae7c81f9f7f404eb3299232c10098958d2
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-03-22 19:35:25 +0200

    Add investments controller

--- app/controllers/investments_controller.rb
+++ app/controllers/investments_controller.rb
@@ -1,6 +1,6 @@
 class InvestmentsController < ApplicationController
   load_and_authorize_resource
-  before_action :set_investment, only: %i[ show edit update destroy ]
+  before_action :set_investment, only: %i[show edit update destroy]
 
   # GET /investments or /investments.json
   def index
@@ -10,8 +10,7 @@ class InvestmentsController < ApplicationController
   end
 
   # GET /investments/1 or /investments/1.json
-  def show
-  end
+  def show; end
 
   # GET /investments/new
   def new
@@ -19,29 +18,24 @@ class InvestmentsController < ApplicationController
   end
 
   # GET /investments/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /investments or /investments.json
   def create
-    @investment = Investment.new(investment_params)
-
-    respond_to do |format|
-      if @investment.save
-        format.html { redirect_to investment_url(@investment), notice: "Investment was successfully created." }
-        format.json { render :show, status: :created, location: @investment }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @investment.errors, status: :unprocessable_entity }
+    params[:investment][:group_id].each do |g|
+      unless g.empty?
+        @investment = Investment.new(investment_params.merge(user_id: current_user.id, group_id: g))
+        @investment.save
       end
     end
+    redirect_to group_investments_path
   end
 
   # PATCH/PUT /investments/1 or /investments/1.json
   def update
     respond_to do |format|
       if @investment.update(investment_params)
-        format.html { redirect_to investment_url(@investment), notice: "Investment was successfully updated." }
+        format.html { redirect_to investment_url(@investment), notice: 'Investment was successfully updated.' }
         format.json { render :show, status: :ok, location: @investment }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -55,19 +49,20 @@ class InvestmentsController < ApplicationController
     @investment.destroy
 
     respond_to do |format|
-      format.html { redirect_to investments_url, notice: "Investment was successfully destroyed." }
+      format.html { redirect_to group_investments_url, notice: 'Investment was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_investment
-      @investment = Investment.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def investment_params
-      params.require(:investment).permit(:name, :amount)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_investment
+    @investment = Investment.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def investment_params
+    params.require(:investment).permit(:name, :amount)
+  end
 end
