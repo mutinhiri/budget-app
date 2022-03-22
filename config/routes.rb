724122b25774615482431a4eb65030528d6f50da
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-03-22 10:05:43 +0200

    Add resources route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  # resources :investments
+  resources :investments
   root 'investments#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
