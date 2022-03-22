7a8866e34885870bcf2805fc1a3fef316e6dbd2c
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-03-22 20:24:50 +0200

    Add migrations and associations

--- db/migrate/20220322181710_add_user_ref_to_investments.rb
+++ db/migrate/20220322181710_add_user_ref_to_investments.rb
@@ -1,5 +1,5 @@
 class AddUserRefToInvestments < ActiveRecord::Migration[7.0]
   def change
-    add_reference :investments, :user, null: false, foreign_key: true
+    add_reference :investments, :user, foreign_key: true
   end
 end
--- db/migrate/20220322181858_add_group_ref_to_investments.rb
+++ db/migrate/20220322181858_add_group_ref_to_investments.rb
@@ -1,5 +1,5 @@
 class AddGroupRefToInvestments < ActiveRecord::Migration[7.0]
   def change
-    add_reference :investments, :group, null: false, foreign_key: true
+    add_reference :investments, :group, foreign_key: true
   end
 end
