0f3efed7fe451b00778339da26c74e3808abfee9
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-03-22 17:41:19 +0200

    After sign out path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(_resource)
     groups_path
   end
+
+  def after_sign_out_path_for(_resource)
+    new_user_session_path
+  end
 end
