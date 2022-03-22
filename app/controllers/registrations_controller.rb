faf9c8736254c99420e4676f797632b5133afe90
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-03-22 10:13:30 +0200

    Add sign_up path

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,1 +1,7 @@
-class Registrations Controller < Devise::RegistrationsController
+class RegistrationsController < Devise::RegistrationsController
+  protected
+
+  def after_sign_up_path_for(_resource)
+    groups_path
+  end
+end
