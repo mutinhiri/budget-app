bd832b115075753ac1518c1fa37b40bd8528bc7b
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-03-22 17:39:17 +0200

    Check page method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :check_page
   before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def check_page
+    session[:current_page] = nil
+  end
 end
