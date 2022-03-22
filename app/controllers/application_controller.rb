de9fbb92df57623057358eec47733bc6bc28cba1
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-03-22 17:42:24 +0200

    Permit user sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,7 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(_resource)
     new_user_session_path
   end
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit()
 end
