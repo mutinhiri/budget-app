1447a26a58dc0457335df1a8d7c9f65fe16896b3
Barnabas Mutinhiri <barnabmutinhiri@gmail.com>
2022-03-23 12:33:42 +0200

    Fix linter errors

--- test/system/investments_test.rb
+++ test/system/investments_test.rb
@@ -1,43 +1,43 @@
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class InvestmentsTest < ApplicationSystemTestCase
   setup do
     @investment = investments(:one)
   end
 
-  test "visiting the index" do
+  test 'visiting the index' do
     visit investments_url
-    assert_selector "h1", text: "Investments"
+    assert_selector 'h1', text: 'Investments'
   end
 
-  test "should create investment" do
+  test 'should create investment' do
     visit investments_url
-    click_on "New investment"
+    click_on 'New investment'
 
-    fill_in "Amount", with: @investment.amount
-    fill_in "Name", with: @investment.name
-    click_on "Create Investment"
+    fill_in 'Amount', with: @investment.amount
+    fill_in 'Name', with: @investment.name
+    click_on 'Create Investment'
 
-    assert_text "Investment was successfully created"
-    click_on "Back"
+    assert_text 'Investment was successfully created'
+    click_on 'Back'
   end
 
-  test "should update Investment" do
+  test 'should update Investment' do
     visit investment_url(@investment)
-    click_on "Edit this investment", match: :first
+    click_on 'Edit this investment', match: :first
 
-    fill_in "Amount", with: @investment.amount
-    fill_in "Name", with: @investment.name
-    click_on "Update Investment"
+    fill_in 'Amount', with: @investment.amount
+    fill_in 'Name', with: @investment.name
+    click_on 'Update Investment'
 
-    assert_text "Investment was successfully updated"
-    click_on "Back"
+    assert_text 'Investment was successfully updated'
+    click_on 'Back'
   end
 
-  test "should destroy Investment" do
+  test 'should destroy Investment' do
     visit investment_url(@investment)
-    click_on "Destroy this investment", match: :first
+    click_on 'Destroy this investment', match: :first
 
-    assert_text "Investment was successfully destroyed"
+    assert_text 'Investment was successfully destroyed'
   end
 end
